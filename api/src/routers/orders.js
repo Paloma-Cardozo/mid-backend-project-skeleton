8732050f758bf66799b0737457e3566f03c4d11e
PalomaCardozo <palomacardozo88@gmail.com>
2026-05-13 09:50:48 +0200

    fix: remove unused checkout import from orders router

--- api/src/routers/orders.js
+++ api/src/routers/orders.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { checkout, listOrders, getOrder } from "#controllers/orders.js";
+import { listOrders, getOrder } from "#controllers/orders.js";
 import { authenticate } from "#middlewares/auth.js";
 
 const ordersRouter = express.Router();
